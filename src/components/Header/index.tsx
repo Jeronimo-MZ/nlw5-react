ba0347c41ac3b973a1b860fa485ced931fe2897e
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-04-22 01:36:33 +0200

    update header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import Link from "next/link";
 
 import styles from "./styles.module.scss";
 
@@ -9,7 +10,11 @@ export function Header() {
     });
     return (
         <header className={styles.headerContainer}>
-            <img src="/logo.svg" alt="Podcastr" />
+            <Link href="/">
+                <a>
+                    <img src="/logo.svg" alt="Podcastr" />
+                </a>
+            </Link>
             <p>O melhor para você ouvir sempre</p>
             <span>{currentDate}</span>
         </header>
