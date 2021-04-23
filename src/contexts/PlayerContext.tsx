8eee974e5679debfc0205306957aaff77a92e3a8
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-04-23 20:15:50 +0200

    refactor player context

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { Children, createContext, useState } from "react";
 type Episode = {
     title: string;
     members: string;
@@ -15,4 +15,46 @@ type PlayerContextData = {
     isPlaying: boolean;
     setPlayingState: (state: boolean) => void;
 };
+
 export const PlayerContext = createContext({} as PlayerContextData);
+
+type PlayerContextProviderProps = {
+    children: React.ReactNode;
+};
+
+export const PlayerContextProvider = ({
+    children,
+}: PlayerContextProviderProps) => {
+    const [episodeList, setEpisodeList] = useState([]);
+    const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    function play(episode: Episode) {
+        setEpisodeList([episode]);
+        setCurrentEpisodeIndex(0);
+        setIsPlaying(true);
+    }
+
+    function togglePlay() {
+        setIsPlaying(!isPlaying);
+    }
+
+    function setPlayingState(state: boolean) {
+        setIsPlaying(isPlaying);
+    }
+
+    return (
+        <PlayerContext.Provider
+            value={{
+                episodeList,
+                currentEpisodeIndex,
+                play,
+                togglePlay,
+                isPlaying,
+                setPlayingState,
+            }}
+        >
+            {children}
+        </PlayerContext.Provider>
+    );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,41 +1,13 @@
 import { Header } from "../components/Header";
 import styles from "../styles/app.module.scss";
 import { Player } from "../components/Player";
-import { PlayerContext } from "../contexts/PlayerContext";
+import { PlayerContextProvider } from "../contexts/PlayerContext";
 
 import "../styles/global.scss";
-import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-    const [episodeList, setEpisodeList] = useState([]);
-    const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-    const [isPlaying, setIsPlaying] = useState(false);
-
-    function play(episode) {
-        setEpisodeList([episode]);
-        setCurrentEpisodeIndex(0);
-        setIsPlaying(true);
-    }
-
-    function togglePlay() {
-        setIsPlaying(!isPlaying);
-    }
-
-    function setPlayingState(state: boolean) {
-        setIsPlaying(isPlaying);
-    }
-
     return (
-        <PlayerContext.Provider
-            value={{
-                episodeList,
-                currentEpisodeIndex,
-                play,
-                togglePlay,
-                isPlaying,
-                setPlayingState,
-            }}
-        >
+        <PlayerContextProvider>
             <div className={styles.appWrapper}>
                 <main>
                     <Header />
@@ -43,7 +15,8 @@ function MyApp({ Component, pageProps }) {
                 </main>
                 <Player />
             </div>
-        </PlayerContext.Provider>
+            ;
+        </PlayerContextProvider>
     );
 }
 
